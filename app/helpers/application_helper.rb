e125d6bb2e095164600af0d101ac422b56444e3c
ricardopnunes <ricardopontin145@gmail.com>
2022-04-19 15:09:10 -0300

    First

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,15 @@
+module ApplicationHelper
+    def date_br(date_us)
+        date_us.strftime("%d-%m-%Y")
+    end
+
+    def ambiente_rails
+        if Rails.env.development?
+            "Desenvolvimento"
+        elsif Rails.env.production?
+            "Produção"
+        else
+            "Testes"
+        end
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+  resources :coins
+  root to: 'welcome#index'
+  
+end
